c7abc816f92d136b72b5f10948a6f0815336b099
serafim7k <mycomputer10j12@gmail.com>
2023-10-26 18:45:41 +0300

    my blog 2022

--- apps/routes.py
+++ apps/routes.py
@@ -1,12 +1,17 @@
-from apps import app
-from flask import render_template, flash, redirect, url_for
-from apps.forms import LoginForm
+from apps import app, db
+from flask import render_template, flash, redirect, url_for, request
+from apps.forms import LoginForm, RegistrationForm
+from flask_login import current_user, login_user, logout_user, login_required
+from apps.models import User
+# from werkzeug.urls import url_parse
+from urllib.parse import urlparse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Yurii'}
+    user = {'username': 'Serafim'}
     posts = [
         {
             'author': {'username': 'David'},
@@ -22,16 +27,48 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        flash(f"Login requested for user{form.username.data}, remember_me{form.remember_me.data}, password{form.password.data}")
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password!")
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlparse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+
+        # flash(f'{user}')
+        # flash(f"Login requested for user{form.username.data}, remember_me{form.remember_me.data}, password{form.password.data}")
         # flash("All GOOD!")
-        return redirect(url_for('index'))
-    return render_template('login.html', title="Sign In", form=form)
+        # return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 # @app.route('/log')
 # def log():
 #     return render_template('base.html', title="LOG")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You REGISTERED')
+    return render_template("register.html", title='Home page', form=form)
 
--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 # print(basedir)
 
+
 # print(os.environ.get('DATABASE_URL'))
 # print('sqlite:///' + os.path.join(basedir, 'app.db'))
 class Config(object):
