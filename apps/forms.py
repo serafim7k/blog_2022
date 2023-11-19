3ac665fa458d1249c059bb96b97b48c97d4d8412
serafim7k <mycomputer10j12@gmail.com>
2023-11-19 18:31:26 +0200

    frofile

--- apps/forms.py
+++ apps/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from apps.models import User
 from email_validator import EmailNotValidError
 
@@ -28,4 +28,10 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError("Enter other email")
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
 
--- apps/routes.py
+++ apps/routes.py
@@ -5,6 +5,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from apps.models import User
 # from werkzeug.urls import url_parse
 from urllib.parse import urlparse
+from datetime import datetime
 
 
 @app.route('/')
@@ -72,3 +73,20 @@ def register():
         flash('You REGISTERED')
     return render_template("register.html", title='Home page', form=form)
 
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': "Text post 1"},
+        {'author': user, 'body': "Text post 2"},
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
